1d1ab2048540f7d3aab4dbc3c348d26c0806e098
GeorgeStevenMarcell <georgemarcell5@gmail.com>
2026-08-17 11:11:20 +0700

    Add Readme.MD

--- src/main/java/com/example/demo/controller/TransactionController.java
+++ src/main/java/com/example/demo/controller/TransactionController.java
@@ -3,15 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.dtos.TransferRequestDTO;
 import com.example.demo.dtos.TransferResponseDTO;
 import com.example.demo.services.TransferService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/v1/transaction")
 public class TransactionController {
 
